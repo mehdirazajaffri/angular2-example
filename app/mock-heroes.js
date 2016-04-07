87b78c106d21c8f46cfbab87ac50e3bb07a47721
Mehdi Raza <mehdi.raza@venturedive.com>
2016-04-07 18:06:42 +0500

    this

---
+++ app/mock-heroes.js
@@ -0,0 +1,23 @@
+System.register([], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    var HEROES;
+    return {
+        setters:[],
+        execute: function() {
+            exports_1("HEROES", HEROES = [
+                { "id": 11, "name": "Mr. Shahzad" },
+                { "id": 12, "name": "Zeeshan" },
+                { "id": 13, "name": "Asim" },
+                { "id": 14, "name": "Baqir Altaf" },
+                { "id": 15, "name": "Mehdi Raza" },
+                { "id": 16, "name": "Ali" },
+                { "id": 17, "name": "Jawad" },
+                { "id": 18, "name": "Dr IQ" },
+                { "id": 19, "name": "Magma" },
+                { "id": 20, "name": "Tornado" }
+            ]);
+        }
+    }
+});
+//# sourceMappingURL=mock-heroes.js.map
---
+++ app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import {Hero} from './hero';
+
+export var HEROES: Hero[] = [
+    {"id": 11, "name": "Mr. Shahzad"},
+    {"id": 12, "name": "Zeeshan"},
+    {"id": 13, "name": "Asim"},
+    {"id": 14, "name": "Baqir Altaf"},
+    {"id": 15, "name": "Mehdi Raza"},
+    {"id": 16, "name": "Ali"},
+    {"id": 17, "name": "Jawad"},
+    {"id": 18, "name": "Dr IQ"},
+    {"id": 19, "name": "Magma"},
+    {"id": 20, "name": "Tornado"}
+];
